446f636ed19523312d11352f3e6784052d084172
りゅう <ryu-ookouchi-20h@stu.kbc.ac.jp>
2024-01-20 20:42:42 +0900

    しゅんと合体

--- Assets/Script/Player/PlayerInstantiate.cs
+++ Assets/Script/Player/PlayerInstantiate.cs
@@ -7,18 +7,18 @@ using UnityEngine;
 public class PlayerInstantiate : MonoBehaviour
 {
     [SerializeField] private DataRetation dataRetation;
-    [SerializeField] private CinemachineTargetGroup cinemachineTargetGroup;
+    //[SerializeField] private CinemachineTargetGroup cinemachineTargetGroup;
 
 
     public GameObject[] characterPrefab;    //CharacterPrefab格納配列
     public Transform[] playerSpawnPos;      //PlayerSpawn位置配列
 
-    public List<PlayerInput> playerList= new List<PlayerInput>();  //作成したプレイヤーをList化する
+    //public List<PlayerInput> playerList= new List<PlayerInput>();  //作成したプレイヤーをList化する
 
     private void Awake()
     {
         dataRetation = GameObject.Find("DataRetation").GetComponent<DataRetation>();    //DataRetation取得
-        cinemachineTargetGroup = GameObject.Find("TargetGroup").GetComponent<CinemachineTargetGroup>(); //CinemachineTargetGroup取得
+        //cinemachineTargetGroup = GameObject.Find("TargetGroup").GetComponent<CinemachineTargetGroup>(); //CinemachineTargetGroup取得
     }
 
     private void Start()
@@ -50,8 +50,8 @@ public class PlayerInstantiate : MonoBehaviour
                     PlayerInput input1 = PlayerInput.Instantiate(characterPrefab[0], -1, null, -1, current);    //生成
                     input1.gameObject.transform.position = playerSpawnPos[i].position;  //位置
                     input1.gameObject.transform.rotation = Quaternion.Euler(0, 180, 0); //回転
-                    playerList.Add(input1); //Listに追加
-                    AddTargetToGroup(input1.gameObject.transform, 1.0f, 10.0f);  //Targetリストに追加
+                    //playerList.Add(input1); //Listに追加
+                    //AddTargetToGroup(input1.gameObject.transform, 1.0f, 10.0f);  //Targetリストに追加
                     break;
                 //サメ
                 case 1:
@@ -60,8 +60,8 @@ public class PlayerInstantiate : MonoBehaviour
                     PlayerInput input2 = PlayerInput.Instantiate(characterPrefab[1], -1, null, -1, current);
                     input2.gameObject.transform.position = playerSpawnPos[i].position;
                     input2.gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
-                    playerList.Add(input2);
-                    AddTargetToGroup(input2.gameObject.transform, 1.0f, 10.0f);
+                    //playerList.Add(input2);
+                    //AddTargetToGroup(input2.gameObject.transform, 1.0f, 10.0f);
                     break;
                 //カメ
                 case 2:
@@ -71,8 +71,8 @@ public class PlayerInstantiate : MonoBehaviour
                     PlayerInput input3 = PlayerInput.Instantiate(characterPrefab[2], -1, null, -1, current);    //生成
                     input3.gameObject.transform.position = playerSpawnPos[i].position;  //位置
                     input3.gameObject.transform.rotation = Quaternion.Euler(0, 180, 0); //回転
-                    playerList.Add(input3); //Listに追加
-                    AddTargetToGroup(input3.gameObject.transform, 1.0f, 10.0f);  //Targetリストに追加
+                    //playerList.Add(input3); //Listに追加
+                    //AddTargetToGroup(input3.gameObject.transform, 1.0f, 10.0f);  //Targetリストに追加
                     break;
                 //マンタ
                 case 3:
@@ -90,15 +90,15 @@ public class PlayerInstantiate : MonoBehaviour
     }
 
     //カメラ追跡用
-    void AddTargetToGroup(Transform targetTransform, float weight, float radius)
-    {
-        //ターゲットを新しく作成し、リストに追加する
-        CinemachineTargetGroup.Target target = new CinemachineTargetGroup.Target();
-        target.target = targetTransform;    //オブジェクト
-        target.weight = weight;             //幅
-        target.radius = radius;             //半径
+    //void AddTargetToGroup(Transform targetTransform, float weight, float radius)
+    //{
+    //    //ターゲットを新しく作成し、リストに追加する
+    //    CinemachineTargetGroup.Target target = new CinemachineTargetGroup.Target();
+    //    target.target = targetTransform;    //オブジェクト
+    //    target.weight = weight;             //幅
+    //    target.radius = radius;             //半径
 
-        //リストに追加
-        cinemachineTargetGroup.AddMember(targetTransform, weight, radius);
-    }
+    //    //リストに追加
+    //    cinemachineTargetGroup.AddMember(targetTransform, weight, radius);
+    //}
 }
