7e9ad03e9ce9869d020d2f592fefd740d59bf36f
りゅう <ryu-ookouchi-20h@stu.kbc.ac.jp>
2024-01-21 17:05:31 +0900

    オブジェクトを非アクティブではなく、追従対象からはずした

--- Assets/Script/Game/DeadLine.cs
+++ Assets/Script/Game/DeadLine.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class DeadLine : MonoBehaviour
 {
     [SerializeField] PlayerInstantiate playerIns;
     [SerializeField] DataRetation dataRetation;
+    [SerializeField] private CinemachineTargetGroup cinemachineTargetGroup;
 
     private void Start()
     {
         playerIns = GameObject.Find("CreatePlayer").GetComponent<PlayerInstantiate>();  //PlayerInstantiate取得
         dataRetation = GameObject.Find("DataRetation").GetComponent<DataRetation>();    //DataRetation取得
+        cinemachineTargetGroup = GameObject.Find("TargetGroup").GetComponent<CinemachineTargetGroup>(); //CinemachineTargetGroup取得
+
     }
 
     //当たり判定（Trigger）
@@ -26,22 +30,25 @@ public class DeadLine : MonoBehaviour
                     //クマノミ
                     case 0:
                         playerIns.playerList.RemoveAt(i);   //Listから削除する
-                        other.gameObject.SetActive(false);  //非アクティブにする
+                        cinemachineTargetGroup.RemoveMember(other.transform);
+                        //other.gameObject.SetActive(false);  //非アクティブにする
                         break;
                     //サメ
                     case 1:
                         playerIns.playerList.RemoveAt(i);   //Listから削除する
-                        other.gameObject.SetActive(false);  //非アクティブにする
+                        cinemachineTargetGroup.RemoveMember(other.transform);
+                        //other.gameObject.SetActive(false);  //非アクティブにする
                         break;
                     //カメ
                     case 2:
                         playerIns.playerList.RemoveAt(i);   //Listから削除する
-                        other.gameObject.SetActive(false);  //非アクティブにする
+                        cinemachineTargetGroup.RemoveMember(other.transform);
+                        //other.gameObject.SetActive(false);  //非アクティブにする
                         break;
                     //マンタ
                     case 3:
                         playerIns.playerList.RemoveAt(i);   //Listから削除する
-                        other.gameObject.SetActive(false);  //非アクティブにする
+                        //other.gameObject.SetActive(false);  //非アクティブにする
                         break;
                 }
             }
